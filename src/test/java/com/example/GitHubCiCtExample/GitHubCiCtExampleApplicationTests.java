9bcb6b4b44b6817923cb69afe648a5296c6d48e7
jaiganesh707 <jaiganeshjkm@gmail.com>
2025-06-18 16:24:56 +0530

    firstCommit

---
+++ src/test/java/com/example/GitHubCiCtExample/GitHubCiCtExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitHubCiCtExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubCiCtExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
